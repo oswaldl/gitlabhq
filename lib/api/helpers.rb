626fb232ddea7533dbe33eeff42636b200487107
oswaldl <oswaldl@126.com>
2014-10-07 12:20:46 +0000

    change search project to have find by name function

--- lib/api/helpers.rb
+++ lib/api/helpers.rb
@@ -46,7 +46,7 @@ module API
     end
 
     def find_project(id)
-      project = Project.find_with_namespace(id) || Project.find_by(id: id)
+      project = Project.find_with_namespace(id) || Project.find_by(id: id) || Project.find_by_name(id)
 
       if project && can?(current_user, :read_project, project)
         project
